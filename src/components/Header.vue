b748e88a1b0f02875855306f853a63404acd728c
waqarsubhani <waqaralisubhani89@gmail.com>
2020-06-01 23:37:52 +0700

    removing unnessary comments

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,10 @@
   <v-app-bar app>
     <v-toolbar-title class="headline text-uppercase mr4">
       <span>News</span>
-      <!-- <span class="font-weight-light">Trader</span> -->
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items>
       <v-btn text to="/history">History</v-btn>
-      <!-- <v-btn text to="/portfolio">Portfolio</v-btn>
-      <v-btn text to="/stocks">Ações</v-btn>-->
     </v-toolbar-items>
 
     <search-toolbar></search-toolbar>
